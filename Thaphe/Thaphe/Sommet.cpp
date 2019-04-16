8876af311249b7b131a25a6d2e83a434fd4aa8c9
Archors <combeantoine78@gmail.com>
2019-04-16 09:52:41 +0200

    Arrete propre dans sommet

--- Thaphe/Thaphe/Sommet.cpp
+++ Thaphe/Thaphe/Sommet.cpp
@@ -16,12 +16,34 @@ Sommet::~Sommet()
 {
 }
 
-
-
 Arete::Arete()
+{}
+
+Arete::Arete(std::string id, Sommet* s1, Sommet* s2, std::vector<float> poids, bool oriente) : m_id(id), m_oriente(oriente), m_poids(poids)
+{
+	m_sommets = std::make_pair(s1, s2);
+	m_poids = poids;
+}
+
+void Arete::Dessiner()
 {
 }
 
+std::pair<const Sommet*, const Sommet*> Arete::getSommets()
+{
+	return std::pair<const Sommet*, const Sommet*>();
+}
+
+const float Arete::getPoids(int indice)
+{
+	return 0.0f;
+}
+
+
+const int Arete::getNombrePoids()
+{
+	return 0;
+}
 
 Arete::~Arete()
 {
--- Thaphe/Thaphe/Sommet.h
+++ Thaphe/Thaphe/Sommet.h
@@ -28,10 +28,10 @@ private:
 	const std::string m_id;
 	std::vector<float> m_poids;
 	std::pair<const Sommet*, const Sommet*> m_sommets; //Si oriente, 1er = départ, 2nd = arrivée
-	bool oriente;
+	bool m_oriente;
 public:
-	Arete(std::string id, Sommet* s1, Sommet* s2, std::vector<const float>, bool);
 	Arete();
+	Arete(std::string id, Sommet* s1, Sommet* s2, std::vector<float> poids, bool oriente);
 	void Dessiner();
 	std::pair<const Sommet*, const Sommet*> getSommets();
 	const float getPoids(int indice);
