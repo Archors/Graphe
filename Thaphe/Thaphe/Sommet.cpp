fb520bf8d17cce467742a061e590832ad61ccdb3
Archor <antoine.combe@edu.ece.fr>
2019-04-16 15:07:21 +0200

    debut prim

--- Thaphe/Thaphe/Sommet.cpp
+++ Thaphe/Thaphe/Sommet.cpp
@@ -16,6 +16,25 @@ void Sommet::Dessiner(ALLEGRO_BITMAP* bmp)
 std::vector<const Arete*> Sommet::Prim(int indicePoids)
 {
 	std::vector<const Arete*> prim;
+	Arete* curent = NULL;
+	int test=0;
+	while (!test) { //Tant qu'on a pas visité tous les sommets
+		for (auto x : m_voisins) //Parcourir tous les sommets adjacents au sommet actuelle
+		{
+			if (curent == NULL) //Si c'est le premier on l'enregistre
+				curent = x.second;
+			else if (curent->getPoids(indicePoids) < x.second->getPoids(indicePoids)) //Sinon on vérifie que c'est le plus proche
+			{
+				for (auto y : prim) //On parcourt les arretes déjà ajoutées
+					//if (y->getSommets().) //On vérifie que le sommet n'est pas déjà dedans
+						curent = x.second; //Si c'est le plus proche on l'ajoute au potentiel nouveau sommet
+			}
+		
+		}
+		prim.push_back(curent); //On ajoute l'arrete la plus courte
+
+
+	}
 	return prim;
 }
 
